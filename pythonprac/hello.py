3e5ec10b3b9701b9a0bca3588f0c01a9a53bd54b
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2023-01-30 21:12:26 +0900

    파이썬의 기초 변수선언/함수/출력/조건문/반복문/

---
+++ pythonprac/hello.py
@@ -0,0 +1,24 @@
+print('hey')
+
+num1=1
+num2=2
+
+def sum(n1,n2):
+    n1+n2
+print(num1+num2)
+
+
+sum(num1,num2)
+
+age=25
+
+
+
+
+ages=[4,19,24,192,34]
+
+for a in ages:
+    if a > 20:
+        print('성인')
+    else:
+        print('청소년')
