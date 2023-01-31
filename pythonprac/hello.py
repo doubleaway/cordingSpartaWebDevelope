9869d2c717cb606e5a0fbadd25ea684eac12cb33
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2023-01-31 21:58:11 +0900

    venv 설치 및 리퀘스트 설치 도중 오류.. 내일까지 해결안되면 질문하기!

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,24 +1,6 @@
-print('hey')
+import requests 
 
-num1=1
-num2=2
+r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
+rjson = r.json()
 
-def sum(n1,n2):
-    n1+n2
-print(num1+num2)
-
-
-sum(num1,num2)
-
-age=25
-
-
-
-
-ages=[4,19,24,192,34]
-
-for a in ages:
-    if a > 20:
-        print('성인')
-    else:
-        print('청소년')
+print(rjson)
