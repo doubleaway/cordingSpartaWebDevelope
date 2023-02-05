a444bcd99886cbcf8e29ef9d8f2791535e19f90c
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2023-02-05 22:46:38 +0900

    크롤링 연습

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -6,4 +6,17 @@ data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
+##old_content > table > tbody > tr:nth-child(2) > td.point
+# #old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
+trs = soup.select('#old_content > table > tbody > tr')
+for tr in trs:
+    title =  tr.select_one('td.title > div > a')
+    rank = tr.select_one('td:nth-child(1) > img')
+    point = tr.select_one('td.point')
+    if a is not None:
+        print(title)
+        print(rank[alt])
+        print(c.text)
+
+
 # 코딩 시작
